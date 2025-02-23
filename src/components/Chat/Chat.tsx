2466ab05bc22a348aef6b3ad884da4001579301c
nurlan1717 <esedovfn3@gmail.com>
2025-02-23 22:36:45 +0400

    fix: chat tsx

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -6,7 +6,7 @@ import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { Terminal, Send, Shield } from "lucide-react";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://gameverse-hub-17.vercel.app", {
     withCredentials: true,
     transports: ["websocket"],
 });
